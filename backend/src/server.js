e483a8c764a65f36fb89ac4a0b819eb5f89eebfb
Lucas <lucasmoc47@gmail.com>
2020-03-02 01:18:21 -0300

    Setting up folder structure and initializing routes file

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 
 const express = require('express')
 
+const routes = require('./routes')
+
 const app = express()
 
+app.use(routes)
 app.listen(process.env.PORT || 3333)
