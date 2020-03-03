d4abb4dadb3a969289cbf8690b5ae4a7e17755ad
Lucas <lucasmoc47@gmail.com>
2020-03-03 16:54:51 -0300

    Adding api methods

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -7,9 +7,17 @@ const api = axios.create({
 })
 
 const getSetlists = () => api.get('/setlists')
+const getSetlistById = (setlist_id) => api.get(`/setlists/${setlist_id}`)
+const createSetlist = (payload) => api.post('/setlists', payload)
+const updateSetlist = (setlist_id, payload) => api.put(`/setlists/${setlist_id}`, payload)
+const deleteSetlist = (setlist_id) => api.delete(`/setlists/${setlist_id}`)
 
 const apis = {
-	getSetlists
+	getSetlists,
+	getSetlistById,
+	createSetlist,
+	updateSetlist,
+	deleteSetlist
 }
 
 export default apis
