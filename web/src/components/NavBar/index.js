4dad8093bca423d662a2c201ba095d6fc7af38ac
Lucas <lucasmoc47@gmail.com>
2020-03-05 18:15:27 -0300

    Adding a functionality to the 'back' button in the NavBar

--- web/src/components/NavBar/index.js
+++ web/src/components/NavBar/index.js
@@ -3,11 +3,11 @@ import React from 'react';
 import './styles.css'
 
 export default function NavBar(props) {
-  return (
-	<nav>
-		<i>{props.leftItem}</i>
-		<h1>{props.title}</h1>
-		<i>logout</i>
-	</nav>
-  );
+	return (
+		<nav>
+			{props.title === "SETLISTS" ? <i>logo</i> : <i onClick={() => props.goBack()}>back</i>}
+			<h1>{props.title}</h1>
+			<i>logout</i>
+		</nav>
+	);
 }
